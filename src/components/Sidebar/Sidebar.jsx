d2c2407185bd6fdbc00434a410a7cd089f29938b
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-09-05 18:03:18 +0300

    switch genre functionality added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
 import { useGetGenresQuery } from '../../services/TMDB';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import genreIcons from '../../assets/genres';
+import logo from '../../FilmPedia.png';
 
-const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
+const redLogo = logo;
+const blueLogo = logo;
 
 const categories = [
   { label: 'Popular', value: 'popular' },
@@ -19,7 +22,10 @@ const categories = [
 function Sidebar({ setMobileOpen }) {
   const theme = useTheme();
   const classes = useStyles();
+  const dispatch = useDispatch();
   const { data, isFetching } = useGetGenresQuery();
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  console.log(genreIdOrCategoryName);
 
   return (
     <>
@@ -35,7 +41,7 @@ function Sidebar({ setMobileOpen }) {
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
-            <ListItem button onClick={() => {}}>
+            <ListItem button onClick={() => dispatch(selectGenreOrCategory(value))}>
               <ListItemIcon>
                 <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
               </ListItemIcon>
@@ -54,7 +60,7 @@ function Sidebar({ setMobileOpen }) {
         )
           : data?.genres?.map(({ name, id }) => (
             <Link key={name} className={classes.links} to="/">
-              <ListItem button onClick={() => {}}>
+              <ListItem button onClick={() => dispatch(selectGenreOrCategory(id))}>
                 <ListItemIcon>
                   <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
                 </ListItemIcon>
