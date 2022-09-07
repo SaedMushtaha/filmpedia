d9d75d8fb1f95e2e7151e1d62ed7411420ffbf14
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-09-07 19:17:19 +0300

    authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
-const Profile = () => {
+function Profile() {
+  const { user } = useSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  const favouriteMovies = [];
+
   return (
-    <div>
-      Profile
-    </div>
-  )
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {favouriteMovies.length === 0
+        ? <Typography variant="h5">Add favourite or watchlist same movies to see them here!</Typography>
+        : <Box>FAVOURITE MOVIES</Box>}
+    </Box>
+  );
 }
 
-export default Profile
+export default Profile;
